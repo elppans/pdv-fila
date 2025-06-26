5dd3d899c8dbd892b29a45af19d533708f950a0b
elppans <elppansmk@gmail.com>
2025-06-26 00:44:23 -0300

    Update asound.testador.sh

--- asound.testador.sh
+++ asound.testador.sh
@@ -151,9 +151,7 @@ resposta=$?
 if [ "$resposta" -eq 0 ]; then
     salvar_como_padrao "$card" "$device"
 else
-    dialog --msgbox "Dispositivo **não** foi salvo como padrão. Voltando ao menu..." 7 50
-    # Deseja testar outro?
-    dialog --yesno "Deseja testar outro dispositivo?" 7 50 || break
+    dialog --yesno "Dispositivo **não** foi salvo como padrão. Voltando ao menu..." 7 50 || continue
 fi
 
 
